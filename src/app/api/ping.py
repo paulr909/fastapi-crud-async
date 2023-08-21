2fe20df19e6bfb948923ee9c05746ffc47f3e309
paulrogers <paulr909@proton.me>
2023-08-21 09:28:30 +0100

    add *.http client methods

--- src/app/api/ping.py
+++ src/app/api/ping.py
@@ -5,6 +5,4 @@ router = APIRouter()
 
 @router.get("/ping")
 async def pong():
-    # Some async operation could happen here
-    # Example: `notes = await get_all_notes()`
     return {"ping": "pong!"}
